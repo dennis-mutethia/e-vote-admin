1fd82715b3b0c290a2e612b72f397869dc0a9012
Dennis Muga <dennis.mutethia1@accesscfa.com>
2025-02-11 16:15:03 +0300

    Update pdf.py

--- pdf.py
+++ pdf.py
@@ -19,17 +19,20 @@ def extract_data_from_pdf(pdf_path):
                     constituency_id = str(uuid.uuid5(uuid.NAMESPACE_DNS, (f'{county_id}-{int(row[2])}')))
                     ward_id = str(uuid.uuid5(uuid.NAMESPACE_DNS, (f'{constituency_id}-{int(row[4])}')))
                     polling_station_id = str(uuid.uuid5(uuid.NAMESPACE_DNS, (f'{ward_id}-{int(row[6])}')))
-                    if county_id != county_id_old:
-                        db.insert_county(county_id, int(row[0]), row[1].upper())
-                        county_id_old = county_id
-                    if constituency_id != constituency_id_old:
-                        db.insert_constituency(constituency_id, int(row[2]), county_id, row[3].upper())
-                        constituency_id_old = constituency_id
-                    if ward_id != ward_id_old:
-                        db.insert_ward(ward_id, int(row[4]), constituency_id, row[5].upper())
-                        ward_id_old = ward_id
+                    # if county_id != county_id_old:
+                    #     db.insert_county(county_id, int(row[0]), row[1].upper())
+                    #     county_id_old = county_id
+                    # if constituency_id != constituency_id_old:
+                    #     db.insert_constituency(constituency_id, int(row[2]), county_id, row[3].upper())
+                    #     constituency_id_old = constituency_id
+                    # if ward_id != ward_id_old:
+                    #     db.insert_ward(ward_id, int(row[4]), constituency_id, row[5].upper())
+                    #     ward_id_old = ward_id
                     if polling_station_id != polling_station_id_old:
-                        db.insert_polling_station(polling_station_id, int(row[6]), ward_id, row[7].upper())
+                        #db.insert_polling_station(polling_station_id, int(row[6]), ward_id, row[7].upper())
+                        db.create_partition('voters', polling_station_id)
+                        db.create_partition('votes', polling_station_id)
+                        db.create_partition('sms_codes', polling_station_id)
                         polling_station_id_old = polling_station_id
 
 if __name__ == '__main__':
